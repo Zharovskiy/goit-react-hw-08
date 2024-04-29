51e6d19d2e83a47f1d405d37c6632b9be706e6e5
Yurii <143278195+Zharovskiy@users.noreply.github.com>
2024-04-30 01:44:09 +0300

    home page

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,5 +1,7 @@
+import css from "./HomePage.module.css";
+
 const HomePage = () => {
-  return <div>Home Page</div>;
+  return <h1 className={css.title}>Contacts book. Welcome to the page</h1>;
 };
 
 export default HomePage;
