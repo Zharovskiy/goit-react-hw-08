4671c6b9a5cd67f545490948c4bbccb3d21d7835
Yurii <143278195+Zharovskiy@users.noreply.github.com>
2024-04-29 12:45:11 +0300

    Update slice.js

--- src/redux/auth/slice.js
+++ src/redux/auth/slice.js
@@ -3,6 +3,7 @@ import { register } from "./operation";
 import { login } from "./operation";
 import { refreshUser } from "./operation";
 import { logout } from "./operation";
+// import { store } from "../store";
 
 const initialState = {
   user: {
@@ -39,6 +40,7 @@ export const authSlice = createSlice({
         state.user = action.payload;
       })
       .addCase(logout.fulfilled, () => {
+        // store.contacts.items = null;
         return initialState;
       })
       .addMatcher(
